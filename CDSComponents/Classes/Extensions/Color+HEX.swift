3d7e1c7981d5d3377efc45752dea1000f1eb0ef0
Elias Ferreira <eliasferreira.pro@gmail.com>
2024-08-27 22:50:56 -0300

    implement hex colors

--- CDSComponents/Classes/Extensions/Color+HEX.swift
+++ CDSComponents/Classes/Extensions/Color+HEX.swift
@@ -9,53 +9,18 @@ import SwiftUI
 
 extension Color {
     init(hex: String) {
-        self.init(uiColor: .init(hex: hex))
-    }
-}
-
-extension UIColor {
-    convenience init(hex: String) {
-        var red: CGFloat = 0.0
-        var green: CGFloat = 0.0
-        var blue: CGFloat = 0.0
-        var alpha: CGFloat = 1.0
+        var cleanHexCode = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        cleanHexCode = cleanHexCode.replacingOccurrences(of: "#", with: "")
+        print(cleanHexCode)
+        var rgb: UInt64 = 0
         
-        let hex = hex.replacingOccurrences(of: "#", with: "")
-        let scanner = Scanner(string: hex)
-        var hexValue: CUnsignedLongLong = 0
-        if scanner.scanHexInt64(&hexValue) {
-            switch hex.count {
-            case 3:
-                red   = CGFloat((hexValue & 0xF00) >> 8) / 15.0
-                green = CGFloat((hexValue & 0x0F0) >> 4) / 15.0
-                blue  = CGFloat(hexValue & 0x00F) / 15.0
-                
-            case 4:
-                alpha = CGFloat((hexValue & 0xF000) >> 12) / 15.0
-                red   = CGFloat((hexValue & 0x0F00) >> 8) / 15.0
-                green = CGFloat((hexValue & 0x00F0) >> 4) / 15.0
-                blue  = CGFloat(hexValue & 0x000F) / 15.0
-            
-            case 6:
-                red   = CGFloat((hexValue & 0xFF0000) >> 16) / 255.0
-                green = CGFloat((hexValue & 0x00FF00) >> 8) / 255.0
-                blue  = CGFloat(hexValue & 0x0000FF) / 255.0
-                
-            case 8:
-                alpha = CGFloat((hexValue & 0xFF000000) >> 24) / 255.0
-                red   = CGFloat((hexValue & 0x00FF0000) >> 16) / 255.0
-                green = CGFloat((hexValue & 0x0000FF00) >> 8) / 255.0
-                blue  = CGFloat(hexValue & 0x000000FF) / 255.0
-                
-            default:
-                red   = 0.0
-                green = 0.0
-                blue  = 0.0
-
-            }
-        }
+        Scanner(string: cleanHexCode).scanHexInt64(&rgb)
         
-        self.init(red: red, green: green, blue: blue, alpha: alpha)
+        let red = Double((rgb >> 16) & 0xFF) / 255.0
+        let green = Double((rgb >> 8) & 0xFF) / 255.0
+        let blue = Double(rgb & 0xFF) / 255.0
+        
+        self.init(red: red, green: green, blue: blue)
     }
 }
 
